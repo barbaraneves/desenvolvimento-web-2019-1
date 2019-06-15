85718f828bbb6384966ad6814b428cb992810b5f
Bárbara Neves <barbara.steph@alu.ufc.br>
2019-06-14 22:21:58 -0300

    Incluindo o campo do tipo Data

--- Atividade 7/Atividade07/src/main/java/atividade7/com/br/model/Pessoa.java
+++ Atividade 7/Atividade07/src/main/java/atividade7/com/br/model/Pessoa.java
@@ -1,9 +1,15 @@
 package atividade7.com.br.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TemporalType;
+import javax.persistence.Temporal;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Pessoa {
@@ -15,6 +21,18 @@ public class Pessoa {
 	private String nome;
 	private String serie;
 	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 	public Long getCodigo() {
 		return codigo;
 	}
